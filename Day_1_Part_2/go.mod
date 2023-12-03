77d1314ba9e9e80a463be077f7187576296236d5
bjle <bjle.business@gmail.com>
2023-12-03 01:16:45 -0500

    copy solution from part 1

---
+++ Day_1_Part_2/go.mod
@@ -0,0 +1,3 @@
+module github.com/bjlePersonalGithub/Advent-Of-Code/Day_2
+
+go 1.21.4
---
+++ Day_1_Part_2/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"strconv"
+	"strings"
+
+	"github.com/bjlePersonalGithub/Advent-Of-Code/Day_2/internal/util"
+)
+
+func main() {
+	scanner, file := util.GetScanner()
+	defer file.Close()
+	total := 0
+	coordinateSlice := []string{}
+	for scanner.Scan() {
+		line := scanner.Text()
+		for i := 0; i < len(line); i++ {
+			if line[i] > 47 && line[i] < 58 {
+				coordinateSlice = append(coordinateSlice, string(line[i]))
+
+				break
+			}
+		}
+		for i := len(line) - 1; i >= 0; i-- {
+			if line[i] > 47 && line[i] < 58 {
+				coordinateSlice = append(coordinateSlice, string(line[i]))
+				break
+			}
+		}
+		if len(coordinateSlice) == 2 {
+			combinedString := strings.Join(coordinateSlice, "")
+
+			coordinate, _ := strconv.Atoi(combinedString)
+			coordinateSlice = []string{}
+			total += coordinate
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Total %d\n", total)
+}
